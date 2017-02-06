0a91c02aa214d5602307a2a53bd9213cff4ddced
Lucie Becz <luciebecz@yahoo.com>
2017-02-06 09:30:05 -0700

    set up of game

---
+++ Assets/javascripts/memory_match.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+
+var buttons = document.getElementById("#button");
+buttons.innerHTML= "<img src= https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwim25nfy_rRAhVE8mMKHVAGA9wQjRwIBw&url=http%3A%2F%2Fwww.pixelstalk.net%2Fabstract-backgrounds-hd%2F&bvm=bv.146094739,d.cGw&psig=AFQjCNFLPXYLqkprgIzNq5tQ0tCXtk84pA&ust=1486439877542001 />";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var memory_array = [A,A,F,F,T,T,Z,Z,Q,Q,L,L];
+// var memory_value = [];
+// var card_ids = [];
+// var cards_flipped = 0;
+
+// Array.prototype.memory_tile_shuffle = function() {
+//   var i =this.length, j, temp;
+//   while(i > 0) {
+//     j = Math.floor(Math.random() * (i+1));
+//     temp= this[j];
+//     this[j] = this[i];
+//     this[i] = temp;
+//   }
+// }
+// function newBoard() {
+//   cards_flipped = 0;
+//   var output = ' ';
+//   memory_array.memory_tile_shuffle();
+//   for(var i = 0; i < memory_array.length; i++){
+//     output += '<div id="card_'+i+"onclick="memoryFlipCard(this,\'')
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
